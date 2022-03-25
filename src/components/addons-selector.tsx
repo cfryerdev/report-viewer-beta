40a0df079d1a5e8f30f81aa54a11156c5a2d07b8
Chris Fryer <cfryerdev@gmail.com>
2022-03-25 09:26:41 -0600

    replaced dropdown with checks, easier for dark/light mode

--- src/components/addons-selector.tsx
+++ src/components/addons-selector.tsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddonsSelector = ({ addon, setAddonCallback }) => {
+  const [selected, setSelected] = useState(addon);
+
+  const executeSelectionChange = (value) => {
+    setAddonCallback(value);
+    setSelected(value);
+  };
+
+  const SelectedCheck = ({ name }) => {
+    return selected === name 
+      ? <i className="fas fa-check-square fa-lg mr-2"></i>
+      : <i className="far fa-square fa-lg mr-2"></i>
+  };
+
   return (
     <div className="card mb-2">
       <div className="card-header card-header-rose">
         <i className="fas fa-puzzle-piece"></i> Addon Handler (Custom Stats)
       </div>
       <div className="card-body">
-        <select
-          className="form-control form-select"
-          id="a_addon"
-          defaultValue={addon}
-          onChange={(e) => setAddonCallback(e.target.value)}
-        >
-          <option value="raw">Raw Data</option>
-          <option value="metrics-by-endpoint">Metrics By Endpoint</option>
-        </select>
+        <div className="radio-selector" onClick={() => { executeSelectionChange('raw')}}>
+          <SelectedCheck name="raw" />
+          Raw Data
+        </div>
+        <div className="radio-selector" onClick={() => { executeSelectionChange('metrics-by-endpoint')}}>
+          <SelectedCheck name="metrics-by-endpoint" />
+          Metrics By Endpoint
+        </div>
       </div>
       <div className="card-footer">
         <span className="stats">
